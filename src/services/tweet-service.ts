e8da7c01c40d033e0fbc5e7fcc477c624594f746
wmattern0 <wm.m.mattern@gmail.com>
2017-03-29 17:53:32 -0400

    stuck

--- src/services/tweet-service.ts
+++ src/services/tweet-service.ts
@@ -6,6 +6,8 @@ export class TweetService{
 
     params: URLSearchParams
     public tweetUrl:string = "http://localhost:8080/svc/v1/tweets";
+    // public tweetUrl:string = " https://project-clean-tweets.herokuapp.com/svc/v1/tweets";
+
 
     constructor(public http:Http){
         this.http = http;
@@ -22,4 +24,13 @@ export class TweetService{
     searchForScore(twitterHandle){//returns just an int
         return this.http.get(this.tweetUrl+"/"+twitterHandle+"/score");
     }
+
+    getBattleResult(awayTwitterHandle, homeTwitterHandle){
+        return this.http.get(this.tweetUrl+"/"+awayTwitterHandle+"/"+homeTwitterHandle+"/100/score");
+    }
+
+    getExpertResult(twitterHandle) {
+      return this.http.get(this.tweetUrl+"/"+twitterHandle+"/score/detail");
+    }
+
 }
